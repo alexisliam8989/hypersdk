7e083e456e3484d72b8fc6abb210a2dafe23f0e1
alexisliam8989 <alexisliam89@gmail.com>
2024-07-20 13:23:23 +0000

    module2

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "LIAMHPR"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "LIAM"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "L"
 )
 
 var ID ids.ID
